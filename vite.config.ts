5c0b3f3892e0fc18a300c8d2add573d3a7486609
nir-zigdon2 <nir.zigdon@anaplan.com>
2025-03-10 23:01:38 +0200

    improve uniq data for nir zigdon

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/trip-agenda-harmony/',
   server: {
     host: "::",
     port: 8080,
